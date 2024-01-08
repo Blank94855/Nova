6380072476abb213750552d8fb116950f54ecbec
Adrien Duermael <adrien@duermael.com>
2024-01-08 21:08:31 +0100

    add test module

---
+++ test/test.lua
@@ -0,0 +1,10 @@
+-- This module does nothing interesting, it only exists for test purposes.
+-- github.com/aduermael/modzh/test
+
+mod = {}
+
+mod.test = function()
+	print("test module: IT WORKS!")
+end
+
+return mod
