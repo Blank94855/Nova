87b25467e45a3283ec228925affa6aae64548929
Adrien Duermael <adrien@duermael.com>
2024-03-04 21:38:25 +0100

    fire optims

--- fire/fire.lua
+++ fire/fire.lua
@@ -27,17 +27,24 @@ local defaultConfig = {
 local tickListener
 local toRemove = {}
 
-local pow = 2
-local up = 0.05
+local up = 0.1
 local upInv = 1.0 / up
 local down = 1.0 - up
 local downInv = 1.0 / down
 
+-- local pow = 2
+-- function scale(p)
+-- 	if p <= up then
+-- 		return (p * upInv) ^ pow
+-- 	end
+-- 	return 1.0 - ((p - up) * downInv) ^ pow
+-- end
+
 function scale(p)
 	if p <= up then
-		return (p * upInv) ^ pow
+		return (math.sin(p * math.pi * upInv - math.pi * 0.5) + 1) * 0.5
 	end
-	return 1.0 - ((p - up) * downInv) ^ pow
+	return (math.sin((p - up) * math.pi * downInv + math.pi * 0.5) + 1) * 0.5
 end
 
 function startTickIfNeeded()
@@ -53,38 +60,40 @@ function startTickIfNeeded()
 		model = Shape(ms)
 	end
 
+	local d = Number3.Zero
+	local l
 	tickListener = LocalEvent:Listen(LocalEvent.Name.Tick, function(dt)
 		for _, source in ipairs(sources) do
 			source.t = source.t + dt
 			if source.t >= source.config.delay then
 				source.t = source.t % source.config.delay
 				local p = table.remove(pool)
+
 				if p == nil then
 					p = Shape(model)
+					p.v = Number3(0, 15, 0)
+					p.rAxis = Number3(math.random(), math.random(), math.random())
+					p.Acceleration = -Config.ConstantAcceleration
+					p.Physics = PhysicsMode.Disabled
 				end
+
 				p:SetParent(World)
-
-				local d = Number3(0, 0, math.random() * source.config.radius)
-				d:Rotate(0, math.random() * math.pi * 2, 0)
-				p.Position = source.Position + d
-
-				p.Acceleration = -Config.ConstantAcceleration
-				p.Scale = 0
-				p.v = Number3(0, 15, 0)
-				p.rAxis = Number3(math.random(), math.random(), math.random())
-				-- p.Physics = PhysicsMode.Dynamic
-				p.Physics = PhysicsMode.Disabled
 				p.life = LIFE
+
+				d:Set(0, 0, math.random() * source.config.radius)
+				d:Rotate(0, math.random() * math.pi * 2, 0)
+				p.Position:Set(source.Position + d)
+				p.Scale:Set(0, 0, 0)
+
 				particles[p] = true
 			end
 		end
 
-		local l
 		for p, _ in pairs(particles) do
 			p.life = p.life - dt
 			l = 1.0 - (p.life / LIFE)
 			p.Scale = scale(l) * SCALE
-			p.Position = p.Position + p.v * dt
+			p.Position:Set(p.Position + p.v * dt)
 			p:RotateWorld(p.rAxis, dt)
 			p.Palette[1].Color:Lerp(START_COLOR, END_COLOR, l)
 			if p.life <= 0 then
